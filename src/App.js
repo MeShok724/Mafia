340bff4d96b458b08210a20e28bc656bacebe6c5
user <mishakopytok@gmail.com>
2024-05-12 15:38:34 +0300

    join room page design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import backgroundImage from './images/bacground1.jpg';
+import backgroundImage from './images/bacground2.jpg';
 
 function App() {
   return (
       <div className="cont" style={{backgroundImage: `url(${backgroundImage})`}}>
-        <h2 className="header">Подключение к комнате</h2>
+        <h2 className="header">ПОДКЛЮЧЕНИЕ К КОМНАТЕ</h2>
         <form className="form-connect">
-            <label className="inp-label">Ваш никнейм:</label>
+            <label className="inp-label">никнейм</label>
             <input type="text" className="inp-name"/>
-            <label className="inp-label">Название комнаты:</label>
+            <label className="inp-label">комната</label>
             <input type="text" className="inp-room"/>
-            <button className="form-btn">Присоединиться</button>
+            <button className="form-btn"><b>ВОЙТИ</b></button>
         </form>
       </div>
   );
