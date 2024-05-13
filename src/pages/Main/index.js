361a60829b7fc9ca84fab09612beacc6366642d2
user <mishakopytok@gmail.com>
2024-05-13 15:33:57 +0300

    new project

---
+++ src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import backgroundImage from '../../images/bacground2.jpg';
+export default function Main(){
+    return (
+        <div className="cont" style={{backgroundImage: `url(${backgroundImage})`}}>
+            <h2 className="header">ПОДКЛЮЧЕНИЕ К КОМНАТЕ</h2>
+            <form className="form-connect">
+                <label className="inp-label">никнейм</label>
+                <input type="text" className="inp-name"/>
+                <label className="inp-label">комната</label>
+                <input type="text" className="inp-room"/>
+                <button className="form-btn"><b>ВОЙТИ</b></button>
+            </form>
+        </div>
+    );
+}
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+
+export default function NotFound404(){
+    return (
+        <div>
+            Not Found 404
+        </div>
+    );
+}
