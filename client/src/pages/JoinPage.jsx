d6380fcc8c97b4329d67cbdda78e77e06637c7c6
Mikhail Kapytok <mishakopytok@gmail.com>
2025-12-10 01:09:36 +0300

    Remove SpeechToText component and clean up index.html

--- client/src/pages/JoinPage.jsx
+++ client/src/pages/JoinPage.jsx
@@ -2,7 +2,6 @@ import '../styles/Join.css';
 import backgroundImage from '../images/background1.jpg';
 import {useState} from "react";
 import {useNavigate} from 'react-router-dom';
-import {SpeechToText} from '../services/SpeechToText'
 
 
 export default function JoinPage(props){
@@ -55,7 +54,6 @@ export default function JoinPage(props){
                     onClick={ToRoomPage}
                 ><b>ВОЙТИ</b></button>
             </div>
-            <SpeechToText/>
         </div>
     );
 }
